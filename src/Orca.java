f74e49de23db192f8c3d825b952a0d8033215810
1937192 <1937192@01-18132.pusd.dom>
2025-05-30 10:41:22 -0700

    added all the cards onto the screen

--- src/Orca.java
+++ src/Orca.java
@@ -5,7 +5,7 @@ import java.awt.Toolkit;
 import java.awt.geom.AffineTransform;
 import java.net.URL;
 
-public class Orca{
+public class Orca extends Card{
 	private Image inFront, backWard, left, right; 	
 	private AffineTransform tx;
 	
