1ddd9f26dafac7fcd71e9870976cfa63b8ef1dad
1937192 <1937192@01-18132.pusd.dom>
2025-06-02 10:37:02 -0700

    fixed some errors

--- src/Card.java
+++ src/Card.java
@@ -18,38 +18,16 @@ public abstract class Card {
         return name;
     }
 
-<<<<<<< HEAD
-    public int getX() { 
-    	return x; 
-    }
-    
-    public void setX(int x) { 
-    	this.x = x; 
-    }
-   
-    public int getY() { 
-    	return y;
-    }
-    
-    public void setY(int y) { 
-    	this.y = y;
-    }
-=======
     public int getDir() {
         return dir;
     }
 
     public int getX() { return x; }
     public void setX(int x) { this.x = x; }
->>>>>>> branch 'lucy-branch' of https://github.com/lucyytrann/OceanMatchingGame.git
 
-    public int getWidth() {
-    	return width; 
-    }
-    
-    public int getHeight() { 
-    	return height;
-    }
-    
-    
+    public int getY() { return y; }
+    public void setY(int y) { this.y = y; }
+
+    public int getWidth() { return width; }
+    public int getHeight() { return height; }
 }
